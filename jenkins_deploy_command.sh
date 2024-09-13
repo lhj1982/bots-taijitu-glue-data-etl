4fb42288e9ec1d16dd9adb3c5ebcaf00c61ab260
Yan Zhou <Yan.Zhou2@nike.com>
2024-09-13 10:39:02 +0800

    GCTAIYI-1839: s3 upload command

--- jenkins_deploy_command.sh
+++ jenkins_deploy_command.sh
@@ -6,33 +6,34 @@ TEMPLATE_FILE="$3"
 STACK_NAME="$4"
 ENV="$5"
 
+echo "script local path: $SCRIPT_LOCAL_PATH"
+echo "script s3 path: $SCRIPT_S3_PATH"
+echo "cloudformation file path: $TEMPLATE_FILE"
+echo "stack name: $STACK_NAME"
+echo "selected env: $ENV"
 
-# 上传模板文件到S3
-upload_s3_output=$(aws s3 cp "$SCRIPT_LOCAL_PATH" "$S3_PATH")
 
-if [ $? -ne 0 ]; then
-  echo "Error uploading template to S3."
-  exit 1
-fi
+aws s3 cp "$SCRIPT_LOCAL_PATH" "$SCRIPT_S3_PATH"
+
 
 # 检查堆栈是否存在
-describe_stacks_output=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" 2>/dev/null)
-
-if [ $? -eq 0 ]; then
-  # 堆栈存在，执行更新操作
-  echo "Updating stack: $STACK_NAME"
-  aws cloudformation update-stack \
-    --region cn-northwest-1 \
-    --stack-name "$STACK_NAME" \
-    --template-body "file://$TEMPLATE_FILE" \
-    --parameters ParameterKey=ENV,ParameterValue="$ENV" ParameterKey=SCRIPT_S3_PATH,ParameterValue="$SCRIPT_S3_PATH" \
-
-else
-  # 堆栈不存在，执行创建操作
-  echo "Creating new stack: $STACK_NAME"
-  aws cloudformation create-stack \
-    --region cn-northwest-1 \
-    --stack-name "$STACK_NAME" \
-    --template-body "file://$TEMPLATE_FILE" \
-    --parameters ParameterKey=ENV,ParameterValue="$ENV" ParameterKey=SCRIPT_S3_PATH,ParameterValue="$SCRIPT_S3_PATH" \
-fi
+#describe_stacks_output=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" 2>/dev/null)
+#
+#if [ $? -eq 0 ]; then
+#  # 堆栈存在，执行更新操作
+#  echo "Updating stack: $STACK_NAME"
+#  aws cloudformation update-stack \
+#    --region cn-northwest-1 \
+#    --stack-name "$STACK_NAME" \
+#    --template-body "file://$TEMPLATE_FILE" \
+#    --parameters ParameterKey=ENV,ParameterValue="$ENV" ParameterKey=SCRIPT_S3_PATH,ParameterValue="$SCRIPT_S3_PATH" \
+#
+#else
+#  # 堆栈不存在，执行创建操作
+#  echo "Creating new stack: $STACK_NAME"
+#  aws cloudformation create-stack \
+#    --region cn-northwest-1 \
+#    --stack-name "$STACK_NAME" \
+#    --template-body "file://$TEMPLATE_FILE" \
+#    --parameters ParameterKey=ENV,ParameterValue="$ENV" ParameterKey=SCRIPT_S3_PATH,ParameterValue="$SCRIPT_S3_PATH" \
+#fi
